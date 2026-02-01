d281447fd318afccd9e4af732a9f87d075eacf7a
youone-its <yuanalbyan13@gmail.com>
2026-02-01 10:50:46 +0700

    kayanya save menu nya ada yang diupdate, saya lupa hehe

--- ui/menus/main_scene.gd
+++ ui/menus/main_scene.gd
@@ -39,7 +39,8 @@ func _on_button_pressed(button_name: String):
 				GameData.is_loading_from_save = false # Pastikan mode bukan loading
 				
 				# Pindah ke scene pertama
-				get_tree().change_scene_to_file("uid://c1owv7wqit87p")
+				#get_tree().change_scene_to_file("uid://c1owv7wqit87p")
+				get_tree().change_scene_to_file("uid://cpw3bnl47ynj1")
 			else:
 				slot_full_dialog.popup_centered()
 		
